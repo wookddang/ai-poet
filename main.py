49234c671e7c852b3c288a8bb7301dbb2f2349ff
RyuSeongWook <121488770+wookddang@users.noreply.github.com>
2025-10-16 00:32:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,37 @@ if st.button('Answer me!'):
              
 
 #st.write('시의 주제는', title)
+st.write("---")
+from datetime import datetime
+
+st.title("💬 댓글 남기기 예제")
+
+# 세션 상태에 댓글 리스트 없으면 초기화
+if "comments" not in st.session_state:
+    st.session_state.comments = []
+
+# 입력 폼
+with st.form("comment_form", clear_on_submit=True):
+    username = st.text_input("이름을 입력하세요")
+    comment = st.text_area("댓글을 남겨주세요")
+    submitted = st.form_submit_button("등록")
+
+    if submitted and username and comment:
+        st.session_state.comments.append({
+            "user": username,
+            "text": comment,
+            "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
+
+# 댓글 출력
+st.write("### 📜 댓글 목록")
+if st.session_state.comments:
+    for c in reversed(st.session_state.comments):
+        st.markdown(f"**{c['user']}** · *{c['time']}*")
+        st.write(c['text'])
+        st.divider()
+else:
+    st.info("아직 댓글이 없습니다. 첫 댓글을 남겨보세요!")
 
 
 
