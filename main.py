47a3f59ed1ee75e4f88f362a43f0660591d2b053
RyuSeongWook <121488770+wookddang@users.noreply.github.com>
2025-10-16 00:23:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 chat_model = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
 
-content = "주제"
+content = "content"
 
 
 
@@ -19,12 +19,13 @@ content = st.text_input('Ask me anything.')
 if st.button('Answer me!'):
     with st.spinner('Wait...'):
 
-        result = chat_model.predict(content + "에 대한 시를 써줘")
+        result = chat_model.predict("Tell me about the" + content)
     
         st.write(result)
              
 
 #st.write('시의 주제는', title)
+
 
 
 
